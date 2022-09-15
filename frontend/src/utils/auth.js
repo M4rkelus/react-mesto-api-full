14fe175fbdc1730f160d1c0ec9ee612666588c39
Mark Pirogov <m4rkpirogov@gmail.com>
2022-09-15 15:35:54 +0300

    change url

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -48,7 +48,6 @@ export const checkToken = (token) => {
   return fetch(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
-      'Accept': 'application/json',
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`,
     },
