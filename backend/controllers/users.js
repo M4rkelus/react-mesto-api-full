65889896cbf63b5a77cd54517ce7b70d1c955c8f
Mark Pirogov <m4rkpirogov@gmail.com>
2022-09-16 16:35:37 +0300

    fix jwt secret
    Signed-off-by: Mark Pirogov <m4rkpirogov@gmail.com>

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,7 +6,7 @@ const BadRequestError = require('../errors/BadRequestError');
 const NotFoundError = require('../errors/NotFoundError');
 const ConflictError = require('../errors/ConflictError');
 
-const { NODE_ENV, JWT_SECRET = 'dev-secret' } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const getUsers = (_, res, next) => {
   User.find({})
